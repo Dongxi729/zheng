edc7438dff32b88969232dfe32caba2c804446c8
zdx <18259129536@163.com>
2016-07-20 15:16:33 +0800

    项目初始化

---
+++ temp/temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  temp
+//
+//  Created by 郑东喜 on 16/7/20.
+//  Copyright © 2016年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ temp/temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  temp
+//
+//  Created by 郑东喜 on 16/7/20.
+//  Copyright © 2016年 郑东喜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
